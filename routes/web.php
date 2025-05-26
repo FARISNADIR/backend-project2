025433d5d33695be48ab275ccf51e51e59e79f9f
FARISNADIR <firdausnasir2010@gmail.com>
2025-05-26 19:23:38 +0800

    Add main page

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/main', function () {
+    return view('main_page');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
